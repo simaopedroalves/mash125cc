e2a333724b05f268e249f0351ebb4190fcf1724f
Simão Pedro <simaaoweb@gmail.com>
2023-05-25 01:57:33 +0100

    My first Project

---
+++ MASH/script.js
@@ -0,0 +1,12 @@
+const icon = document.getElementById('icon');
+const body = document.getElementsByTagName('body');
+
+
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    
+}
+
+$(document).ready( function () {
+    
+})
